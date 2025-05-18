fa0108758feb14ebbfd88fd31555eced2b76e18c
Jacob W <jacobwang092@hotmail.com>
2025-05-19 01:33:47 +0800

    change a little

--- Maplestory_login/Template/TypeMap.cpp
+++ Maplestory_login/Template/TypeMap.cpp
@@ -6,5 +6,24 @@
 //
 
 #include "TypeMap.hpp"
+#include <typeindex>
+#include <unordered_map>
+#include <memory>
+namespace ms {
+
+    template <typename T>
+    class TypeMap{
+    public:
+        using t_ptr = typename std::unique_ptr<T>;
+        using pair = typename std::pair<std::type_index, t_ptr>;
+        using cpair = typename std::pair<const std::type_index, t_ptr>;
+        using underlying_map = typename std::unordered_map<std::type_index, t_ptr>;
+        
+        
+        
+    private:
+        underlying_map container;
+    };
+}
 
 
