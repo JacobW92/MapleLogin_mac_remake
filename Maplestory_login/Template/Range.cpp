bf3860a732d1469b6dcb06b640f97d2981bad7df
Jacob W <jacobwang092@hotmail.com>
2025-05-19 09:12:23 +0800

    Add the Range.cpp

---
+++ Maplestory_login/Template/Range.cpp
@@ -0,0 +1,8 @@
+//
+//  Range.cpp
+//  Maplestory_login
+//
+//  Created by Jacob on 5/19/25.
+//
+
+#include "Range.hpp"
---
+++ Maplestory_login/Template/Range.hpp
@@ -0,0 +1,13 @@
+//
+//  Range.hpp
+//  Maplestory_login
+//
+//  Created by Jacob on 5/19/25.
+//
+
+#ifndef Range_hpp
+#define Range_hpp
+
+#include <stdio.h>
+
+#endif /* Range_hpp */
