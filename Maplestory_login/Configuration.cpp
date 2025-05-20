8ca0f9180ec0b65b1072b8333e5e15df02f43c3f
Jacob W <jacobwang092@hotmail.com>
2025-05-20 09:28:23 +0800

    something fix.

---
+++ Maplestory_login/Configuration.cpp
@@ -0,0 +1,8 @@
+//
+//  Configuration.cpp
+//  Maplestory_login
+//
+//  Created by Jacob on 5/20/25.
+//
+
+#include "Configuration.hpp"
---
+++ Maplestory_login/Configuration.hpp
@@ -0,0 +1,13 @@
+//
+//  Configuration.hpp
+//  Maplestory_login
+//
+//  Created by Jacob on 5/20/25.
+//
+
+#ifndef Configuration_hpp
+#define Configuration_hpp
+
+#include <stdio.h>
+
+#endif /* Configuration_hpp */
